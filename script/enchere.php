f41a0aab9a9e8a0c011399252597c2573b9ea35e
VilainPtiZombie <boutet.maximilien@gmail.com>
2020-01-27 18:38:59 +0100

    Add form int équiepemtn
    Ajout de l'équipement et des valeurs de ce dernier pour la liste d'enchere

--- script/enchere.php
+++ script/enchere.php
@@ -11,7 +11,7 @@ if (isset($_POST) && empty($user_id)) {
 	$user_id = htmlspecialchars($_POST['add_auct_id']);
 	$transfert = $_POST['Auct_transfert'];
 	$desc = htmlspecialchars($_POST['Auct_txt']);
-	$p_start = $_POST['Auct_prix_start'];
+	$pos = $_POST['Auct_last_Pos'];
 	$p_go = $_POST['Auct_prix_sell'];
 	$fm =  $_POST['auct_fm'];
 	$tim = date("Y-m-d H:i:s");
@@ -21,7 +21,7 @@ echo  date("Y-m-d H:i:s");
 // var_dump((is_numeric($fm)) == TRUE && (!empty($fm)));
 // die();
 
-	$sql = $bdd->prepare('INSERT INTO `list_auct`(`user_id`, `auct_serv`, `auct_tranfert`,`auct_screen`,`Auct_txt`,`Auct_prix_start`,`Auct_prix_sell`, `Auct_Equip`, `auct_time`, `auct_fm`, `auct_activ`) VALUES (:u,:a_s,:a_t,:a_sc,:a_txt,:a_p_s,:a_p_go,:a_equip,:tim,:fm,:activ)');
+	$sql = $bdd->prepare('INSERT INTO `list_auct`(`user_id`, `auct_serv`, `auct_tranfert`,`auct_screen`,`Auct_txt`,`auct_last_pos`,`Auct_prix_sell`, `Auct_Equip`, `auct_time`, `auct_fm`, `auct_activ`) VALUES (:u,:a_s,:a_t,:a_sc,:a_txt,:a_p_s,:a_p_go,:a_equip,:tim,:fm,:activ)');
 
 	if ((is_numeric($user_id)) == TRUE && (!empty($user_id))) {
 
@@ -53,9 +53,9 @@ echo  date("Y-m-d H:i:s");
 				$desc = "none";
 				$sql->bindParam('a_txt', $desc, PDO::PARAM_STR);
 			}
-			if ((is_numeric($p_start)) == TRUE && (!empty($p_start))) {
+			if ((isset($pos)) == TRUE && (!empty($pos))) {
 
-				$sql->bindParam('a_p_s', $p_start, PDO::PARAM_STR);
+				$sql->bindParam('a_p_s', $pos, PDO::PARAM_STR);
 
 				if ((is_numeric($p_go)) == TRUE && (!empty($p_go))) {
 
@@ -75,7 +75,10 @@ echo  date("Y-m-d H:i:s");
 					$sql->bindParam('tim', $tim, PDO::PARAM_STR);
 					$sql->bindParam('activ', $activ, PDO::PARAM_STR);
 					
+					var_dump($_POST);
+					
 					$sqlReq = $sql->execute();
+
 					$errorUrl = "Enchère crée !";
 					Header('Refresh:0; url=../tool/enchere.php?err_message='.$errorUrl.'');
 					}else{
@@ -96,8 +99,8 @@ echo  date("Y-m-d H:i:s");
 			Header('Refresh:0; url=../tool/enchere.php?err_message='.$errorUrl.'');
 		}
 	}else{
-		$errorUrl = 'equipementInvalid';
-		Header('Refresh:0; url=../tool/enchere.php?err_message='.$errorUrl.'');
+		$errorUrl = "Profil non trouver";
+		Header('Refresh:0; url=../login.php?err_message='.$errorUrl.'');
 	}
 
 
@@ -105,7 +108,134 @@ echo  date("Y-m-d H:i:s");
 	$errorUrl = 'enchereInvalide';
 	Header('Refresh:0; url=../tool/enchere.php?err_message='.$errorUrl.'');
 }
-if ($sqlReq == TRUE) {
+
+$sql_item = $bdd->prepare('
+	INSERT INTO `list_auct_stats`(`item_id`, `user_id`, `auct_id`, `vie`, `fo`, `ine`, `cha`, `age`, `sa`, `pa`, `pm`, `invo`, `do`, `do_feu`, `do_fo`, `do_cha`, `do_neu`, `do_air`, `do_sort`, `do_arme`, `do_mel`, `do_dis`, `so`, `pp`, `ini`, `pods`, `tac`, `fui`, `ret_pa`, `ret_pm`, `esq_pa`, `esq_pm`, `do_cri`, `do_pou`, `res_cri`, `res_pou`, `pc_neu`, `pc_fo`, `pc_ine`, `pc_cha`, `pc_air`, `res_neu`, `res_fo`, `res_ine`, `res_eau`, `res_air`, `pc_sort`, `pc_arme`, `pc_melee`, `pc_dist`, `renvoie`, `crit`) 
+	VALUES (
+	:item_id
+	,:user_id
+	,:auct_id
+	,:vie
+	,:fo
+	,:ine
+	,:cha
+	,:age
+	,:sa
+	,:pa
+	,:pm
+	,:invo
+	,:do
+	,:do_feu
+	,:do_fo
+	,:do_cha
+	,:do_neu
+	,:do_air
+	,:do_sort
+	,:do_arme
+	,:do_mel
+	,:do_dis
+	,:so
+	,:pp
+	,:ini
+	,:pods
+	,:tac
+	,:fui
+	,:ret_pa
+	,:ret_pm
+	,:esq_pa
+	,:esq_pm
+	,:do_cri
+	,:do_pou
+	,:res_cri
+	,:res_pou
+	,:pc_neu
+	,:pc_fo
+	,:pc_ine
+	,:pc_cha
+	,:pc_air
+	,:res_neu
+	,:res_fo
+	,:res_ine
+	,:res_eau
+	,:res_air
+	,:pc_sort
+	,:pc_arme
+	,:pc_melee
+	,:pc_dist
+	,:renvoie
+	,:crit
+)');
+
+$stat_array = array(
+array( 'vie' , $_POST['vie']),
+array( 'fo' , $_POST['fo']),
+array( 'ine' , $_POST['ine']),
+array( 'cha' , $_POST['cha']),
+array( 'age' , $_POST['age']),
+array( 'sa' , $_POST['sa']),
+array( 'pa' , $_POST['pa']),
+array( 'pm' , $_POST['pm']),
+array( 'invo' , $_POST['invo']),
+array( 'do' , $_POST['do']),
+array( 'do_feu' , $_POST['do_feu']),
+array( 'do_fo' , $_POST['do_fo']),
+array( 'do_cha' , $_POST['do_cha']),
+array( 'do_neu' , $_POST['do_neu']),
+array( 'do_air' , $_POST['do_air']),
+array( 'do_sort' , $_POST['do_sort']),
+array( 'do_arme' , $_POST['do_arme']),
+array( 'do_mel' , $_POST['do_mel']),
+array( 'do_dis' , $_POST['do_dis']),
+array( 'so' , $_POST['so']),
+array( 'pp' , $_POST['pp']),
+array( 'ini' , $_POST['ini']),
+array( 'pods' , $_POST['pods']),
+array( 'tac' , $_POST['tac']),
+array( 'fui' , $_POST['fui']),
+array( 'ret_pa' , $_POST['ret_pa']),
+array( 'ret_pm' , $_POST['ret_pm']),
+array( 'esq_pa' , $_POST['esq_pa']),
+array( 'esq_pm' , $_POST['esq_pm']),
+array( 'do_cri' , $_POST['do_cri']),
+array( 'do_pou' , $_POST['do_pou']),
+array( 'res_cri' , $_POST['res_cri']),
+array( 'res_pou' , $_POST['res_pou']),
+array( 'pc_neu' , $_POST['pc_neu']),
+array( 'pc_fo' , $_POST['pc_fo']),
+array( 'pc_ine' , $_POST['pc_ine']),
+array( 'pc_cha' , $_POST['pc_cha']),
+array( 'pc_air' , $_POST['pc_air']),
+array( 'res_neu' , $_POST['res_neu']),
+array( 'res_fo' , $_POST['res_fo']),
+array( 'res_ine' , $_POST['res_ine']),
+array( 'res_eau' , $_POST['res_eau']),
+array( 'res_air' , $_POST['res_air']),
+array( 'pc_sort' , $_POST['pc_sort']),
+array( 'pc_arme' , $_POST['pc_arme']),
+array( 'pc_melee' , $_POST['pc_melee']),
+array( 'pc_dist' , $_POST['pc_dist']),
+array( 'renvoie' , $_POST['renvoie']),
+array( 'crit' , $_POST['crit']),
+);
+var_dump($stat_array);
+$id_equip = $bdd->lastInsertId();
+
+$sql_item->bindParam('item_id', $equipement, PDO::PARAM_STR);
+$sql_item->bindParam('user_id', $user_id, PDO::PARAM_STR);
+$sql_item->bindParam('auct_id', $id_equip, PDO::PARAM_STR);
+
+for ($row = 0; $row < 49; $row++) {
+	if (empty($stat_array[$row][1])) {
+		$valueN = '';
+		$sql_item->bindParam($stat_array[$row][0], $valueN, PDO::PARAM_STR);
+	}else{
+		$sql_item->bindParam($stat_array[$row][0], $stat_array[$row][1], PDO::PARAM_STR);
+	}
+}
+$sqlReq_item = $sql_item->execute();
+
+
+if (isset($sqlReq)) {
 	//TRAITEMENT IMPORT IMAGE VIA FORMULAIRE
 	//si on a recu un fichier et qu'il y a une erreur
 
--- tool/add-enchere.php
+++ tool/add-enchere.php
@@ -10,7 +10,7 @@
 				</div>
 			<div class="modal-body">
 			    <div class="col-12 input-group">
-			      <select name="Auct_Add_Equip" class="selectpicker form-control" id="serveur" data-live-search="true" data-title="Item à vendre" data-style="text" data-size="10" >
+			      <select required name="Auct_Add_Equip" class="selectpicker form-control" id="serveur" data-live-search="true" data-title="Item à vendre" data-style="text" data-size="10" >
 			      	<?php require ('../script/bdd.php');
 						$sql = "SELECT id, equip_name FROM list_equipement";
 						$req = $bdd->query($sql);
@@ -24,7 +24,7 @@
 			    </div>
 			    <div class="form-row col-12">
 				    <div class="col-4 input-group">
-				      	<select name="Auct_Serv" class="form-control">
+				      	<select required name="Auct_Serv" class="form-control">
 				      		<option selected>Votre Serveur</option>
 					      	<?php require ('../script/bdd.php');
 								$sql3 = "SELECT id, nom_serv FROM serveurs";
@@ -41,11 +41,11 @@
 						</select>
 				    </div>
 			    	<div class="col-4 custom-control custom-checkbox">
-					  	<input name="Auct_transfert" type="checkbox" class="custom-control-input" id="customCheck1" data-toggle="popover" data-trigger="focus" title="Informations" data-content="Prix minimum désiré en kamas">
+					  	<input name="Auct_transfert" type="checkbox" class="custom-control-input" id="customCheck1">
 					 	<label class="custom-control-label" for="customCheck1">Transfert inclus ?</label>
 					</div>
 				    <div class="col-4">
-					<select type="radio" name="auct_fm" class="form-control" placeholder="FM/Exo ?">
+					<select required type="radio" name="auct_fm" class="form-control" placeholder="FM/Exo ?">
 						<option selected>FM/Exo ?</option>
 						<option value="0">Non</option>
 						<option value="1">Oui</option>
@@ -71,15 +71,73 @@
 			    ?>
 			    <div class="col-12  input-group">
 
-			    	<input name="Auct_prix_start" type="text" class="col-6 form-control" placeholder="Prix de départ*" data-toggle="popover" data-trigger="focus" title="Informations" data-placement="left" data-content="Prix de départ de l'enchères en kamas">
-			    	<input name="Auct_prix_sell" type="text" class="col-6 form-control" placeholder="Prix de cession" data-toggle="popover" data-trigger="focus" title="Informations" data-content="Prix minimum désiré en kamas">
+			    	<input required name="Auct_last_Pos" type="text" class="col-4 form-control" placeholder="Position de l'item*" data-toggle="popover" data-trigger="focus" title="Informations" data-placement="left" data-content="Position de l'item, Place marchande ou HDV / Format : [00,00]">
+			    	<input required name="Auct_prix_sell" type="number" class="col-8 form-control" placeholder="Prix de cession" data-toggle="popover" data-trigger="focus" title="Informations" data-content="Prix minimum désiré en kamas">
 			    </div>		    
 			
-		    </div>
+		    
+		    </br>
+			</br>
+			<div class="input-group">
+			<h5>Stat de l'équipement :<small>(non obligatoire mais fortement conseillé)</small></h5>
+			<div class="row col-12">	
+				<input name="vie" placeholder="Vitalité" value="vie" type="number" class="col-3 form-control">	
+				<input name="fo" placeholder="Force" value="fo" type="number" class="col-3 form-control"> 
+				<input name="ine" placeholder="Intelligence" value="ine" type="number" class="col-3 form-control"> 
+				<input name="cha" placeholder="Chance" value="cha" type="number" class="col-3 form-control"> 
+				<input name="age" placeholder="Agilité" value="age" type="number" class="col-3 form-control"> 
+				<input name="sa" placeholder="Sagesse" value="sa" type="number" class="col-3 form-control"> 
+				<input name="pa" placeholder="PA" value="pa" type="number" class="col-3 form-control"> 
+				<input name="pm" placeholder="PM" value="pm" type="number" class="col-3 form-control"> 
+				<input name="invo" placeholder="Invocation" value="invo" type="number" class="col-3 form-control"> 
+				<input name="do" placeholder="Dommages" value="do" type="number" class="col-3 form-control"> 
+				<input name="do_feu" placeholder="Dmg Feu" value="do_feu" type="number" class="col-3 form-control"> 
+				<input name="do_air" placeholder="Dmg Air" value="do_air" type="number" class="col-3 form-control">
+				<input name="do_fo" placeholder="Dmg Terre" value="do_fo" type="number" class="col-3 form-control"> 
+				<input name="do_cha" placeholder="Dmg Chance" value="do_cha" type="number" class="col-3 form-control">
+				<input name="do_neu" placeholder="Dmg Neutre" value="do_neu" type="number" class="col-3 form-control">
+				<input name="do_sort" placeholder="% Dmg Sorts" value="do_sort" type="number" class="col-3 form-control"> 
+				<input name="do_arme" placeholder="% Dmg Armes" value="do_arme" type="number" class="col-3 form-control"> 
+				<input name="do_mel" placeholder="% Dmg Mélée" value="do_mel" type="number" class="col-3 form-control"> 
+				<input name="do_dis" placeholder="% Dmg Distance" value="do_dis" type="number" class="col-3 form-control"> 
+				<input name="so" placeholder="Soin" value="so" type="number" class="col-3 form-control"> 
+				<input name="pp" placeholder="Prospection" value="pp" type="number" class="col-3 form-control">
+				<input name="ini" placeholder="Initiative" value="ini" type="number" class="col-3 form-control">
+				<input name="pods" placeholder="Pods" value="pods" type="number" class="col-3 form-control">
+				<input name="tac" placeholder="Tacle" value="tac" type="number" class="col-3 form-control">
+				<input name="fui" placeholder="Fuite" value="fui" type="number" class="col-3 form-control">
+				<input name="ret_pa" placeholder="Ret. PA" value="ret_pa" type="number" class="col-3 form-control">
+				<input name="ret_pm" placeholder="Ret. PM" value="ret_pm" type="number" class="col-3 form-control">
+				<input name="esq_pa" placeholder="Esq. PA" value="esq_pa" type="number" class="col-3 form-control">
+				<input name="esq_pm" placeholder="Esq. PM" value="esq_pm" type="number" class="col-3 form-control">
+				<input name="do_cri" placeholder="Dmg Critique" value="do_cri" type="number" class="col-3 form-control">
+				<input name="do_pou" placeholder="Dmg Poussée" value="do_pou" type="number" class="col-3 form-control">
+				<input name="res_cri" placeholder="Rés Critique" value="res_cri" type="number" class="col-3 form-control">
+				<input name="res_pou" placeholder="Rés Poussée" value="res_pou" type="number" class="col-3 form-control">
+				<input name="pc_neu" placeholder="% Rés. Neutre" value="pc_neu" type="number" class="col-3 form-control">
+				<input name="pc_fo" placeholder="% Rés. Terre" value="pc_fo" type="number" class="col-3 form-control">
+				<input name="pc_ine" placeholder="% Rés. Feu" value="pc_ine" type="number" class="col-3 form-control">
+				<input name="pc_cha" placeholder="% Rés. Eau" value="pc_cha" type="number" class="col-3 form-control">
+				<input name="pc_air" placeholder="% Rés. Air" value="pc_air" type="number" class="col-3 form-control">
+				<input name="res_neu" placeholder="Rés. Neutre" value="res_neu" type="number" class="col-3 form-control">
+				<input name="res_fo" placeholder="Rés. Terre" value="res_fo" type="number" class="col-3 form-control">
+				<input name="res_ine" placeholder="Rés. Feu" value="res_ine" type="number" class="col-3 form-control">
+				<input name="res_eau" placeholder="Rés. Eau" value="res_eau" type="number" class="col-3 form-control">
+				<input name="res_air" placeholder="Rés. Air" value="res_air" type="number" class="col-3 form-control">
+				<input name="pc_sort" placeholder="% Rés Sorts" value="pc_sort" type="number" class="col-3 form-control">
+				<input name="pc_arme" placeholder="% Rés Armes" value="pc_arme" type="number" class="col-3 form-control">
+				<input name="pc_melee" placeholder="% Rés Mélée" value="pc_melee" type="number" class="col-3 form-control">
+				<input name="pc_dist" placeholder="% Rés Distance" value="pc_dist" type="number" class="col-3 form-control">
+				<input name="renvoie" placeholder="Renvoi Dmg" value="renvoie" type="number" class="col-3 form-control">
+				<input name="crit" placeholder="Critique" value="crit" type="number" class="col-3 form-control">
+			</div>
+		</div>
+	</div>
 			<div class="modal-footer">
 				<button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
-				<input type="submit" class="btn btn-primary" name="Add_Auct" value="Mettre en vente">
+				<input type="submit" class="btn btn-primary" name=="Add_Auct" value="Mettre en vente">
 			</div>
+
 		</form>
     </div>
   </div>
